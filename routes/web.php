5088aef1a57e7e26ff8d5b38a2d87ef3cb7992e3
Maksim Grin <maks.grin.2016@gmail.com>
2021-08-15 19:10:29 +0300

    breeze starter kit

--- routes/web.php
+++ routes/web.php
@@ -13,44 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', function () {
-    return view('users');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/status', function () {
-    return view('status');
-});
-
-Route::get('/create_user', function () {
-    return view('create_user');
-});
-
-Route::get('/profile', function () {
-    return view('page_profile');
-});
-
-Route::get('/security', function () {
-    return view('security');
-});
-
-Route::get('/register', function () {
-    return view('page_register');
-});
-
-Route::get('/avatar', function () {
-    return view('media');
-});
-
-Route::get('/login', function () {
-    return view('page_login');
-});
-
-Route::get('/edit', function () {
-    return view('edit');
-});
-
-
+require __DIR__.'/auth.php';
